8ef10139a989a7e5fa52e78f911859fb582fe842
leeeeeoy <hoheho18@naver.com>
2024-01-31 20:02:45 +0900

    add: env 파일 설정

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,11 +1,21 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:leeeeeoy_portfolio/config/firebase_config.dart';
 import 'package:leeeeeoy_portfolio/data/local/app_database.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class AppConfig {
   static Future<void> initConfig() async {
     await initializeFirebase();
+    await dotenv.load(fileName: '.env');
+
+    await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL'] ?? '',
+      anonKey: dotenv.env['SUPABASE_API_KEY'] ?? '',
+      debug: false,
+    );
 
     await Hive.initFlutter();
     await Hive.openBox<bool>(AppDatabase.themeBoxKey);
@@ -13,5 +23,9 @@ abstract class AppConfig {
 }
 
 Future<void> catchError(dynamic error, StackTrace? stack) async {
+  if (kIsWeb) {
+    return;
+  }
+
   FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
 }
--- lib/feature/main/main_screen.dart
+++ lib/feature/main/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key, required this.navigationShell});
@@ -14,7 +15,10 @@ class MainScreen extends StatelessWidget {
         title: const Text('MainScreen AppBar'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              final result = Supabase.instance.client.storage.from('app-metadata').getPublicUrl('bpp/bpp_icon.png');
+              debugPrint(result);
+            },
             icon: const Icon(CupertinoIcons.moon),
           ),
         ],
