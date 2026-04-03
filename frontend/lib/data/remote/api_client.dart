8081f879d1f9a9bfac9a287e10da666e4ccc953d
leeeeeoy <hoheho18@naver.com>
2026-04-03 22:57:17 +0900

    feat: Api 로깅 추가

--- frontend/lib/data/remote/api_client.dart
+++ frontend/lib/data/remote/api_client.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:leeeeeoy_portfolio/config/app_env.dart';
+import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
+import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 
 @lazySingleton
 class ApiClient {
@@ -13,6 +15,16 @@ class ApiClient {
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 10),
         headers: {'Accept': 'application/json'},
+      ),
+    );
+
+    dio.interceptors.add(
+      TalkerDioLogger(
+        settings: const TalkerDioLoggerSettings(
+          printRequestHeaders: true,
+          printResponseHeaders: true,
+          printResponseMessage: true,
+        ),
       ),
     );
   }
